bf10e3d5de1ca48c38ce05e36b81b3d7ec58f13d
improve777 <improve777@gmail.com>
2020-11-14 13:34:39 +0900

    add TrendingEntityTest.kt using AndroidJunit4

---
+++ app/src/androidTest/java/com/improve777/tmdb/data/local/models/TrendingEntityTest.kt
@@ -0,0 +1,52 @@
+package com.improve777.tmdb.data.local.models
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.improve777.tmdb.data.local.AppDatabase
+import com.improve777.tmdb.data.local.dao.TrendingDao
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
+import kotlin.jvm.Throws
+
+@RunWith(AndroidJUnit4::class)
+internal class TrendingEntityTest {
+    private lateinit var trendingDao: TrendingDao
+    private lateinit var db: AppDatabase
+
+    @Before
+    fun createDb() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(
+            context,
+            AppDatabase::class.java,
+        ).build()
+        trendingDao = db.trendingDao()
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        db.close()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun writeAndReadTrending() {
+        val id = 82856
+        val trending = TrendingEntity(id)
+
+        runBlocking {
+            trendingDao.insert(trending)
+            val byId = trendingDao.findById(id)
+
+            assertTrue(id == byId.id)
+        }
+    }
+}
