08c812846a5742d872f0aabff25bc700d33600d8
improve777 <improve777@gmail.com>
2020-11-15 10:01:46 +0900

    update AppDatabase version

--- app/src/main/java/com/improve777/tmdb/data/local/AppDatabase.kt
+++ app/src/main/java/com/improve777/tmdb/data/local/AppDatabase.kt
@@ -2,10 +2,27 @@ package com.improve777.tmdb.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.improve777.tmdb.data.local.dao.TrendingDao
 import com.improve777.tmdb.data.local.models.TrendingEntity
 
-@Database(entities = [TrendingEntity::class], version = 1)
+@Database(entities = [TrendingEntity::class], version = 2)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun trendingDao(): TrendingDao
+
+    companion object {
+        val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL(
+                    "CREATE TABLE trending_new (id INTEGER PRIMARY KEY NOT NULL, name TEXT NOT NULL)"
+                )
+                database.execSQL(
+                    "INSERT INTO trending_new (id, name) SELECT id, '' FROM trending"
+                )
+                database.execSQL("DROP TABLE trending")
+                database.execSQL("ALTER TABLE trending_new RENAME TO trending")
+            }
+        }
+    }
 }
